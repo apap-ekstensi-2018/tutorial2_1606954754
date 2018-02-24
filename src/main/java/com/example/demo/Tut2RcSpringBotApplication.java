071c6f5a8ebf647cc5d4e830f99192489a53fa30
blankdubz <blankdubz@gmail.com>
2018-02-24 15:33:05 +0700

    push file tut2 #1

---
+++ src/main/java/com/example/demo/Tut2RcSpringBotApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tut2RcSpringBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tut2RcSpringBotApplication.class, args);
+	}
+}
